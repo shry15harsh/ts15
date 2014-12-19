190eb3f8a54ec7f9332024aec1e66009e50d1025
Luv <luv94.karakoti@gmail.com>
2014-12-19 21:05:19 +0530

    There are problems with this code, the animations aren't working fluid. Plus I have not been able to work out a method to make these circles of wifi concentric, as I have used pure JS to translate them instead of hardcoding. To jump directly to the rotating line part, comment line 54 and uncomment line 55 of ruler.js

--- luv/js/ruler.js
+++ luv/js/ruler.js
@@ -2,13 +2,11 @@ var screen_size;
 var offset;
 var INTERVAL = 600;
 var RULER_LEN = 1000;
-var CM_LEN = 50;
-var ruler_width = 800;
-var prev_left = 0;//used to set positions for the centimeter marks
-var j = 1;//for animating the millimter marks
-var prev_left_mm;
+var PREV_LEFT = 0;//used to set positions for the marks
 var ZERO_ANIM_INTER = 20;
-var color=['lime','red','magenta','aqua','coral'];
+var NUM_OF_MARKS = 36;
+var RADIUS = [470,430,410];
+var WIDTH = 15*NUM_OF_MARKS;
 $(document).ready(function(){
 	screen_size = $(window).width();
 	offset = screen_size/2;
@@ -51,41 +49,136 @@ function setRuler()
 	$('.ruler').css('top',offset*0.60);
 	$('.ruler').css('left',offset*0.50);
 	$('.ruler').css('display','block');
-	$('.horizLine').animate({"width":ruler_width+"px"});
+	$('.horizLine').animate({"width":15*(NUM_OF_MARKS)+"px"});
 	$('.markings').css('bottom','0px');
-	//$('.markings').css('left',RULER_LEN/20+"px");
-	setMarkings(1);
+	makeMarks();
+	//rotateLine();//comment this out later on
 }
 
-function setMarkings(i)
+function makeMarks()
 {
-	if(i<=6)
+	for(var i = 1;i<=NUM_OF_MARKS;i++)
 	{
-		setTimeout(function(){
-			$('.markings .centimeter:nth-child('+i+')').css('bottom','0px');	
-			$('.markings .centimeter:nth-child('+i+')').css('left',prev_left+'px');
+		$('.ruler .markings').append('<div class = "centimeter"></div>');
+	}
+	for(var i = 1;i<=NUM_OF_MARKS;i++)
+	{
+		$('.markings .centimeter:nth-child('+i+')').css('left',PREV_LEFT);
+		PREV_LEFT+=15;
+	}
+	setMarks(1);
+}
+
+function setMarks(i)
+{
+	if(i<=NUM_OF_MARKS)
+	{
+		setTimeout(function(){	
 			$('.markings .centimeter:nth-child('+i+')').css('display','block');
-			$('.markings .centimeter:nth-child('+i+')').animate({"height":"50px"});
-			setTimeout(setMilliMarks(1,i),INTERVAL);
-			prev_left = parseInt($('.markings .centimeter:nth-child('+i+')').css('left'))+120;
-			setTimeout(setMarkings(i+1),INTERVAL);
-		},2*INTERVAL);
+			if(i%6==1){
+				$('.markings .centimeter:nth-child('+i+')').animate({"height":"50px"});
+			}
+			else{
+				$('.markings .centimeter:nth-child('+i+')').animate({"height":"20px"});
+			}
+			setTimeout(setMarks(i+1),INTERVAL/8);
+		},INTERVAL/4);
+	}
+	if(i>NUM_OF_MARKS)
+	{
+		fadeMarks(1);
 	}
 }
 
-function setMilliMarks(j,i)
+function fadeMarks(i)
 {
-	if(j<=5)
+	if(i<=NUM_OF_MARKS)
 	{
-		if(j%5==1)prev_left_mm = 0;
-		setTimeout(function(){
-			$('.markings .centimeter:nth-child('+i+') .smaller .millimeter:nth-child('+j+')').css('bottom','-50px');
-			$('.markings .centimeter:nth-child('+i+') .smaller .millimeter:nth-child('+j+')').css('left',prev_left_mm+20+'px');
-			$('.markings .centimeter:nth-child('+i+') .smaller .millimeter:nth-child('+j+')').css('display','block');
-			$('.markings .centimeter:nth-child('+i+') .smaller .millimeter:nth-child('+j+')').animate({"height":"20px"});
-			//$('.markings .centimeter:nth-child('+i+') .smaller .millimeter:nth-child('+j+')').css({"background":color[i-1]});
-			prev_left_mm+=20;
-			setTimeout(setMilliMarks(j+1,i),INTERVAL/8);
-		},INTERVAL/8);
+		setTimeout(function(){	
+			$('.markings .centimeter:nth-child('+i+')').animate({"height":"0px"});
+			setTimeout(fadeMarks(i+1),INTERVAL/8);
+		},INTERVAL/4);
+	}
+	if(i>NUM_OF_MARKS)
+	{
+		rotateLine();
+	}
+}
+
+function rotateLine()
+{
+	setTimeout(function(){
+	$('.horizLine').css('transform','rotate(-45deg)');
+	$('.ruler').css('transform','translateY(-100px)');
+	$('.horizLine').css('opacity','0.4');
+	setTimeout(showWaves(),INTERVAL/8);
+	},INTERVAL/4);
+}
+
+function showWaves()
+{
+	var x = [0,15*NUM_OF_MARKS];
+	
+	//insert two circles here, absolute in position
+	for(var i = 1;i<=2;i++)
+	{
+		$('.ruler .wifi').append('<div class = "circle"></div>');
+	}
+	
+	//these two circles are for waves
+	for(var i = 1;i<=6;i++)
+	{
+		$('.ruler .comm').append('<div class = "wave"></div>');
+	}
+	
+	for(var i = 1;i<=2;i++)
+	{
+		$('.wifi .circle:nth-child('+i+')').css('display','block');	
+		$('.wifi .circle:nth-child('+i+')').css('left',x[i-1]+'px');	
+	}
+	
+	for(var i = 1;i<=6;i++)
+	{		
+		if(i<=3)//i.e. the ones down there, we have waves 1,2,3 down there
+			{
+				$('.comm .wave:nth-child('+i+')').addClass('wave2');
+				$('.comm .wave:nth-child('+i+')').css('left',x[0]);
+			}
+		else
+			{
+				$('.comm .wave:nth-child('+i+')').addClass('wave1');
+				$('.comm .wave:nth-child('+i+')').css('left',x[1]);
+			}
+	}
+	
+	var TX = WIDTH * (1-Math.cos(45*Math.PI/180)) / 2;
+	var TY = WIDTH * Math.sin(45*Math.PI/180) / 2;
+	$('.wifi').css('transform','translateY(-16px)');
+	$('.comm').css('transform','translate(-210px,-250px)');
+		
+	for(var i = 1;i<=2;i++)
+	{	
+		$('.wifi .circle:nth-child('+i+')').css({"transform":"translate("+TX*Math.pow(-1,i+1)+"px,"+TY*Math.pow(-1,i+1)+"px)"});
+		$('.wifi .circle:nth-child('+i+')').fadeIn(200);
+	}
+	
+	for(var i = 1;i<=6;i++)
+	{
+		if(i<=3)//make them align to down
+		{
+			$('.comm .wave:nth-child('+i+')').css({"transform":"translate("+TX+"px,"+TY+"px)"});
+		}
+		else
+		{
+			$('.comm .wave:nth-child('+i+')').css({"transform":"translate("+(TX*-1)+"px,"+(TY*-1)+"px)"});
+		}
+	}
+	
+	$('.zeroAbove').css('opacity','0');
+	
+	for(var j = 1;j<=6;j++)
+	{
+		$('.comm .wave:nth-child('+j+')').animate({"height":RADIUS[(j-1)%3],"width":RADIUS[(j-1)%3]},{duration:100,queue:false});
+		$('.comm .wave:nth-child('+j+')').fadeIn(800);								
 	}
 }
