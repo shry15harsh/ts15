2057066c7ba9d693b755628ed4f74870ef333afc
Luv <luv94.karakoti@gmail.com>
2014-12-22 15:27:16 +0530

    The animation with the wifi. Updated

--- luv/js/ruler.js
+++ luv/js/ruler.js
@@ -5,7 +5,6 @@ var RULER_LEN = 1000;
 var PREV_LEFT = 0;//used to set positions for the marks
 var ZERO_ANIM_INTER = 20;
 var NUM_OF_MARKS = 36;
-var RADIUS = [470,430,410];
 var WIDTH = 15*NUM_OF_MARKS;
 $(document).ready(function(){
 	screen_size = $(window).width();
@@ -86,23 +85,19 @@ function setMarks(i)
 	}
 	if(i>NUM_OF_MARKS)
 	{
-		fadeMarks(1);
+		fadeMarks();
 	}
 }
 
-function fadeMarks(i)
+function fadeMarks()
 {
-	if(i<=NUM_OF_MARKS)
-	{
-		setTimeout(function(){	
-			$('.markings .centimeter:nth-child('+i+')').animate({"height":"0px"});
-			setTimeout(fadeMarks(i+1),INTERVAL/8);
-		},INTERVAL/4);
-	}
-	if(i>NUM_OF_MARKS)
-	{
-		rotateLine();
-	}
+	setTimeout(function(){
+		for(var i = 1;i<=NUM_OF_MARKS;i++)
+		{
+			$('.markings .centimeter:nth-child('+i+')').animate({"height":"0px"},{queue:false});
+		}
+		if(i>NUM_OF_MARKS){rotateLine()};
+	},INTERVAL/4);
 }
 
 function rotateLine()
@@ -111,74 +106,32 @@ function rotateLine()
 	$('.horizLine').css('transform','rotate(-45deg)');
 	$('.ruler').css('transform','translateY(-100px)');
 	$('.horizLine').css('opacity','0.4');
-	setTimeout(showWaves(),INTERVAL/8);
-	},INTERVAL/4);
+	$('.zeroAbove').css('opacity','0');
+	},INTERVAL/2);
+	setTimeout(showWaves(),INTERVAL);
 }
 
 function showWaves()
 {
-	var x = [0,15*NUM_OF_MARKS];
-	
-	//insert two circles here, absolute in position
+	var x = [155,15*NUM_OF_MARKS-10];
+	var y = [125,-245];
 	for(var i = 1;i<=2;i++)
 	{
-		$('.ruler .wifi').append('<div class = "circle"></div>');
+		var iota = Math.pow(-1,i+1);
+		$('.comm .Wifi:nth-child('+i+')').css('left',x[i-1]);
+		$('.comm .Wifi:nth-child('+i+')').css('top',y[i-1]);
+		$('.comm .Wifi:nth-child('+i+')').css({"transform":"scale(0.5,"+iota*0.5+") rotate("+45*iota+"deg)"});
+		$('.comm .Wifi:nth-child('+i+')').css({"-webkit-transform":"scale(0.5,"+iota*0.5+") rotate("+45*iota+"deg)"});
 	}
-	
-	//these two circles are for waves
-	for(var i = 1;i<=6;i++)
+	for(var i =1;i<=2;i++)
 	{
-		$('.ruler .comm').append('<div class = "wave"></div>');
+		$('.comm .Wifi:nth-child('+i+')').fadeIn(1000);
 	}
-	
-	for(var i = 1;i<=2;i++)
+	for(var i =1;i<=2;i++)
 	{
-		$('.wifi .circle:nth-child('+i+')').css('display','block');	
-		$('.wifi .circle:nth-child('+i+')').css('left',x[i-1]+'px');	
+		$('.comm .Wifi:nth-child('+i+')').fadeOut(1000*6);
 	}
-	
-	for(var i = 1;i<=6;i++)
-	{		
-		if(i<=3)//i.e. the ones down there, we have waves 1,2,3 down there
-			{
-				$('.comm .wave:nth-child('+i+')').addClass('wave2');
-				$('.comm .wave:nth-child('+i+')').css('left',x[0]);
-			}
-		else
-			{
-				$('.comm .wave:nth-child('+i+')').addClass('wave1');
-				$('.comm .wave:nth-child('+i+')').css('left',x[1]);
-			}
-	}
-	
-	var TX = WIDTH * (1-Math.cos(45*Math.PI/180)) / 2;
-	var TY = WIDTH * Math.sin(45*Math.PI/180) / 2;
-	$('.wifi').css('transform','translateY(-16px)');
-	$('.comm').css('transform','translate(-210px,-250px)');
-		
-	for(var i = 1;i<=2;i++)
-	{	
-		$('.wifi .circle:nth-child('+i+')').css({"transform":"translate("+TX*Math.pow(-1,i+1)+"px,"+TY*Math.pow(-1,i+1)+"px)"});
-		$('.wifi .circle:nth-child('+i+')').fadeIn(200);
-	}
-	
-	for(var i = 1;i<=6;i++)
-	{
-		if(i<=3)//make them align to down
-		{
-			$('.comm .wave:nth-child('+i+')').css({"transform":"translate("+TX+"px,"+TY+"px)"});
-		}
-		else
-		{
-			$('.comm .wave:nth-child('+i+')').css({"transform":"translate("+(TX*-1)+"px,"+(TY*-1)+"px)"});
-		}
-	}
-	
-	$('.zeroAbove').css('opacity','0');
-	
-	for(var j = 1;j<=6;j++)
-	{
-		$('.comm .wave:nth-child('+j+')').animate({"height":RADIUS[(j-1)%3],"width":RADIUS[(j-1)%3]},{duration:100,queue:false});
-		$('.comm .wave:nth-child('+j+')').fadeIn(800);								
-	}
+	setTimeout(function(){$('.ruler').animate({"opacity":"0"});
+		//circle to draw an Earth
+	},6050);
 }
