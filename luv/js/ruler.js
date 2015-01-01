aad5e957a6562733701921019dff47fc26f33078
Harsh <shry.harsh@gmail.com>
2015-01-02 02:13:19 +0530

    document.ready changed to window.load for external load

--- luv/js/ruler.js
+++ luv/js/ruler.js
@@ -11,7 +11,7 @@ var WIDTH = 15*NUM_OF_MARKS;
 var alphas = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','Z'];
 
 
-$(document).ready(function(){
+$(window).load(function(){
 	loadMusic();
 });
 
