5eaa28cdf2d497e2dc360f7b9120623b25535814
Luv <luv94.karakoti@gmail.com>
2015-01-17 20:46:37 +0530

    Events page, made Chrome friendly

--- luv/js/preview.js
+++ luv/js/preview.js
@@ -6,12 +6,13 @@ $('.document').ready(function(){
 		var isFilled = checkForm();
 		if(isFilled == true)
 		{
-			$('.form1 .ename').text($('.eform .ename').val());
-			$('.form1 .edesc').text($('.eform .desc').val());
-			$('.form1 .evenue').text($('.eform .venue').val());
-			$('.form1 .ecoord').text($('.eform .coord1').val()+", "+$('.eform .coord2').val());
-			$('.form1 .edate').text($('.eform .dateC').val());
-			$('.form1').slideDown("slow");
+			$('.previewForm .ename').text($('.eventForm .ename').val());
+			$('.previewForm .edesc').text($('.eventForm .desc').val());
+			$('.previewForm .evenue').text($('.eventForm .venue').val());
+			$('.previewForm .ecoord').text($('.eventForm .coord1').val()+", "+$('.eventForm .coord2').val());
+			$('.previewForm .edate').text($('.eventForm .dateC').val());
+			$('html,body').animate({scrollTop:"750px"},"slow");
+			$('.previewForm').slideDown("slow");
 		}
 	});
 }
@@ -19,21 +20,21 @@ $('.document').ready(function(){
 
 function resetForm()
 {
-	$('.eform')[0].reset();
+	$('.eventForm')[0].reset();
 }
 function checkForm()
 {
 	var isEmpty = false;
-	if($('.eform .ename').val()==""){$('.enamef').addClass("field error");isEmpty = true;}
-	if($('.eform .desc').val()==""){$('.edescf').addClass("field error");isEmpty = true;}
-	if($('.eform .venue').val()==""){$('.evenue').addClass("field error");isEmpty = true;}
-	if($('.eform .coord1').val()=="" && $('.eform .coord2').val()==""){$('.coord').addClass("field error");isEmpty = true;}
-	if($('.eform .dateC').val()==""){$('.edate').addClass("field error");isEmpty = true;}
+	if($('.eventForm .ename').val()==""){$('.enamef').addClass("field error");isEmpty = true;}
+	if($('.eventForm .desc').val()==""){$('.edescf').addClass("field error");isEmpty = true;}
+	if($('.eventForm .venue').val()==""){$('.evenue').addClass("field error");isEmpty = true;}
+	if($('.eventForm .coord1').val()=="" && $('.eventForm .coord2').val()==""){$('.coord').addClass("field error");isEmpty = true;}
+	if($('.eventForm .dateC').val()==""){$('.edate').addClass("field error");isEmpty = true;}
 	if(isEmpty==true)
 	{
 		//show a div at the top with some fields missing error
 		$('html,body').animate({scrollTop:"0px"},"slow");
-		$('.errmess').slideDown();
+		$('.errorMessage').slideDown();
 		return false;
 	}
 	else return true;
