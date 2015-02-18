7b174e5ce52ded7659900d27ef6ed7a7d334cd32
Harsh <shry.harsh@gmail.com>
2015-02-19 05:11:09 +0530

    changed server.js

--- server.js
+++ server.js
@@ -15,6 +15,19 @@ app.use(compression());
 
 app.use(express.static(__dirname + '/', { maxAge : cacheTime }));
 
+app.listen(80, function(){
+	console.log('Listening on 80');
+/*	fs.readFile(__dirname+'/page_visit', {encoding: 'utf-8'}, function(err, data){
+		if(err){
+			page_hit = 0;
+		}else{
+			page_hit = parseInt(data,10);
+		}
+	});
+*/
+});
+
+
 /*app.get('/', function(req, res){
 	++page_hit;
 	fs.writeFile(__dirname+'/page_visit', page_hit);
@@ -66,16 +79,4 @@ app.get('/doraemon', function(req, res){
 });
 app.post('/addmyevent', function(req, res){
 	var event_obj = req.body;
-});
-
-app.listen(80, function(){
-	console.log('Listening on 80');
-/*	fs.readFile(__dirname+'/page_visit', {encoding: 'utf-8'}, function(err, data){
-		if(err){
-			page_hit = 0;
-		}else{
-			page_hit = parseInt(data,10);
-		}
-	});
-*/
 });
